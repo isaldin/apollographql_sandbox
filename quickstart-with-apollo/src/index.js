b88f59647977958f9fb06bcdaedec20db34d8968
saldin <saldin.iliya@gmail.com>
2017-12-01 13:39:54 +0300

    Add batching for queries.

--- quickstart-with-apollo/src/index.js
+++ quickstart-with-apollo/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
+import { BatchHttpLink } from "apollo-link-batch-http";
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import 'tachyons'
 
@@ -11,7 +11,9 @@ import ListPage from './components/ListPage';
 // import CreatePost from './components/CreatePost';
 import './index.css';
 
-const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjamlrgux1wbw0113587gjirq' })
+const httpLink = new BatchHttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cjamlrgux1wbw0113587gjirq',
+});
 
 const client = new ApolloClient({
   link: httpLink,
